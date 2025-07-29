0ed84d0165963616c9da06df3324323daf5abebc
SyedMaazAli-Sasquatch <maaz@sasquatchinnovations.ca>
2025-07-30 03:29:39 +0500

    height

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-white px-2">
-      <div className="w-full max-w-[400px] h-[90vh] rounded-3xl shadow-xl flex flex-col overflow-hidden">
+      <div className="w-full max-w-[400px] h-[80vh] rounded-3xl shadow-xl flex flex-col overflow-hidden">
         
         {/* Scrollable content section */}
         <div className="h-full flex-1 overflow-y-auto scrollbar-none">
